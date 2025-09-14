b292522e5ddf12bead87bf2959567c51c5b2ea5c
NikolaevMikhailRoma <nukolaevmihael7@gmail.com>
2025-09-14 10:53:00 +0400

    refactor output

--- src/youtube_downloader.py
+++ src/youtube_downloader.py
@@ -1,6 +1,23 @@
 import os
 import yt_dlp
 
+class MyLogger:
+    def debug(self, msg):
+        if "has already been downloaded" in msg:
+            print("  File already downloaded.")
+        elif msg.startswith('[Merger]'):
+            print('\n  Converting to mp4...')
+
+    def info(self, msg):
+        if msg.startswith('[Merger]'):
+            print('\n  Converting to mp4...')
+
+    def warning(self, msg):
+        pass
+
+    def error(self, msg):
+        print(msg)
+
 def _get_playlist_title(url):
     ydl_opts = {
         'quiet': True,
@@ -15,7 +32,7 @@ def _get_playlist_title(url):
         except:
             return 'Unknown_Playlist'
 
-def _download_video(url, output_folder, low_quality):
+def _download_video(url, output_folder, low_quality, file_num, total_files):
     if low_quality:
         format_option = 'bestvideo[height<=360]+bestaudio/best[height<=360]'
     else:
@@ -33,6 +50,21 @@ def _download_video(url, output_folder, low_quality):
         outtmpl = os.path.join(output_folder, '%(playlist_index)s_%(title)s.%(ext)s')
     else:
         outtmpl = os.path.join(output_folder, '%(title)s.%(ext)s')
+
+    printed_filename = False
+    def _progress_hook(d):
+        nonlocal printed_filename
+        if d['status'] == 'downloading':
+            if not printed_filename:
+                filename = os.path.basename(d['info_dict'].get('_filename', ''))
+                print(f"File {file_num}/{total_files}: {filename}")
+                printed_filename = True
+            
+            percent_str = d.get('_percent_str', '---.-%').strip()
+            speed_str = d.get('_speed_str', '----.-B/s').strip()
+            print(f'  Downloading: {percent_str} at {speed_str}   ', end='\r')
+        elif d['status'] == 'finished':
+            pass
     
     ydl_opts = {
         'format': format_option,
@@ -45,6 +77,10 @@ def _download_video(url, output_folder, low_quality):
         'noplaylist': noplaylist,
         'restrictfilenames': False,
         'throttled-rate': '1M',
+        'logger': MyLogger(),
+        'progress_hooks': [_progress_hook],
+        'no_warnings': True,
+        'noprogress': True,
     }
 
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
@@ -71,11 +107,12 @@ def run_downloader(low_quality: bool = False, urls_file: str = "youtube_urls.txt
 
     os.makedirs(download_dir, exist_ok=True)
 
-    for url in urls:
+    total_urls = len(urls)
+    for i, url in enumerate(urls):
         print(f"Downloading: {url}")
         try:
-            _download_video(url, download_dir, low_quality)
+            _download_video(url, download_dir, low_quality, i + 1, total_urls)
         except Exception as e:
             print(f"Failed to download {url}. Error: {e}")
 
-    print("Download finished!")
+    print("\nDownload finished!")
